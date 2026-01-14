da376b88de0083f463c069174e32c8fbbb27901a
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-14 17:49:21 -0600

    Time: 4 ms (14.83%), Space: 22.2 MB (38.55%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        int n = asteroids.size();
+
+        stack<int> st;
+
+        for (int i=0; i<n; i++) {
+            if (asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            } else {
+                while (!st.empty() && ((st.top() > 0) && (abs(asteroids[i]) > st.top()))) {
+                    st.pop();
+                }
+                if (st.empty() || st.top() < 0) {
+                    st.push(asteroids[i]);
+                }
+                if (st.top() == abs(asteroids[i])) {
+                    st.pop();
+                }
+            }
+        }
+
+        vector<int> ans;
+        while (!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
