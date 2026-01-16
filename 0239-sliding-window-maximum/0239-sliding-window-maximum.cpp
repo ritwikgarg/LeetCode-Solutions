dbaad456d5c8d3fa168136a3b042c000107d73e2
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-16 15:20:47 -0600

    Time: 11 ms (96.91%), Space: 139.2 MB (66.73%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-    //Idea: for each element, we ask can this element be the max element in any k-length window that it is a part of?
-    //For this, we need the pleIdx and nleIdx for each element
-        if (k==1) {
-            return nums;
-        }
+        // Idea: maintain a doubly-ended queue, wich acts as a monotonic stack (largest element at bottom, smallest
+        // element at top). Whenever we exceed window size, pop from the bottom. Always push from the top, as long
+        // as element being pushed is smaller than element currently at top.
+
+        deque<int> dqIdx;
         int n = nums.size();
-        
-        vector<int> nleIdx = nextLargerElementIndex(nums);
         vector<int> ans;
 
-        int j=0;
-        for (int left=0; left<=n-k; left++) {
-            int right = left + k - 1;
-            if (j < left) j = left;
-
-            while (nleIdx[j] <= right) j = nleIdx[j];
-
-            ans.push_back(nums[j]);
-        }
-
-        return ans;
-    }
-
-    vector<int> nextLargerElementIndex(vector<int>& nums) {
-        int n = nums.size();
-        stack<int> st;
-        vector<int> nleIdx (n, 0);
-
-        for (int i=n-1; i>=0; i--) {
-            while (!st.empty() && nums[st.top()] < nums[i]) {
-                st.pop();
+        for (int i=0; i<n; i++) {
+            while (!dqIdx.empty() && nums[dqIdx.back()] < nums[i]) {
+                dqIdx.pop_back();
             }
-            nleIdx[i] = (st.empty()) ? n : st.top();
-            st.push(i);
+            if (!dqIdx.empty() && dqIdx.front() < i-k+1) {
+                dqIdx.pop_front();
+            }
+            if (i >= k-1) {
+                int largestElementInWindowIdx = (dqIdx.empty()) ? i : dqIdx.front();
+                ans.push_back(nums[largestElementInWindowIdx]);
+            }
+            dqIdx.push_back(i);
         }
-
-        return nleIdx;
+        return ans;
     }
 };
