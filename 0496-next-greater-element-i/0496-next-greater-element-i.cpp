62a898fe42e190e6f5f2a69e75da9de5739f585f
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-17 19:45:38 -0600

    Time: 0 ms (100%), Space: 12.9 MB (35.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> monotonicStack;
-        unordered_map<int, int> elems;
-
-        int m = nums1.size();
-        int n = nums2.size();
-
-        for (int i=0; i<m; i++) {
-            elems.insert({nums1[i], 0}); //Inserting into set for O(1) lookups
-        }
-
-        // Populating the monotonic stack
-        for (int i=n-1; i>=0; i--) {
-            
-            while (monotonicStack.empty() != true && nums2[i] >= monotonicStack.top()) {
-                monotonicStack.pop();
-            }
-
-            if ((monotonicStack.empty() == true) && (elems.find(nums2[i]) != elems.end())) {
-                elems[nums2[i]] = -1;
-            } else if (elems.find(nums2[i]) != elems.end()) {
-                elems[nums2[i]] = monotonicStack.top();
-            }
-
-            monotonicStack.push(nums2[i]);
-        }
-
+        unordered_map<int, int> numbers;
+        stack<int> st;
         vector<int> ans;
-        for (int it: nums1) {
-            ans.push_back(elems[it]);
+
+        for (int i=0; i<nums1.size(); i++) {
+            numbers[nums1[i]] = 0;
+        }
+
+        int n = nums2.size();
+        
+        for (int i=n-1; i>=0; i--) {
+            while(!st.empty() && st.top() < nums2[i]) {
+                st.pop();
+            }
+
+            if(numbers.find(nums2[i]) != numbers.end()) {
+                numbers[nums2[i]] = (st.empty()) ? -1 : st.top();
+            }
+
+            st.push(nums2[i]);
+        }
+
+        for (int i=0; i<nums1.size(); i++) {
+            ans.push_back(numbers[nums1[i]]);
         }
 
         return ans;
