03360474cd43c656678548a01341018132af785d
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-20 17:28:03 -0600

    Time: 7 ms (63.89%), Space: 11.5 MB (43.58%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,49 +1,45 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map <char, int> need; // freq map of chars in t
-        unordered_map <char, int> have; // freq map of chars in s
-
+        unordered_map <char, int> freqMap;
         int m = s.size();
         int n = t.size();
 
+        int req = 0;
         for (int i=0; i<n; i++) {
-            need[t[i]]++;
+            freqMap[t[i]]++; // Initialize a map which contains the counts of char required in s that are in t
+            req++;
         }
 
-        int left = 0;
+        int left = 0, leftIdx = -1;
         int right = 0;
-        int required = need.size(); // no. of distinct characters in need;
-        int formed = 0; // no. ofs characters we have gathered in s, that are needed in t.
-
-        int leftIdx = -1;
-        int rightIdx = -1;
+        int have = 0; // Keeps a track of how many chars in curr substring in s contains chars that are in t
         int minLen = INT_MAX;
+
         while (right < m) {
             char ch = s[right];
-            have[ch]++;
-            if (need.find(ch)!=need.end() && have[ch] == need[ch]) {
-                formed++;
+            freqMap[ch]--;
+            if (freqMap[ch] >= 0) {
+                have++;
             }
-            while (formed >= required) {
-                if ((right-left+1) <= minLen) {
-                    minLen = right-left+1;
-                    rightIdx = right;
-                    leftIdx = left;
+
+            while (have >= req) {
+                int len = right-left+1;
+                if (minLen >= len) {
+                    minLen = len;
+                    leftIdx = left; 
                 }
-                have[s[left]]--;
-                if (need.find(s[left]) != need.end() && have[s[left]] < need[s[left]]) {
-                    formed--;
+                freqMap[s[left]]++;
+                if (freqMap[s[left]] > 0) {
+                    have--;
                 }
                 left++;
             }
             right++;
         }
 
-        if (leftIdx == -1) {
-            return "";
-        }
-        return s.substr(leftIdx, minLen);
+        if (leftIdx == -1) return "";
 
+        return s.substr(leftIdx, minLen);
     }
 };
