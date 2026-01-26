63b9189fd4a220e5775b89fd88998087d3a66cec
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-26 17:19:53 -0600

    Time: 0 ms (100%), Space: 10.5 MB (26.77%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,67 +1,71 @@
 class Solution {
 public:
     int splitArray(vector<int>& nums, int k) {
-        // Idea: BS will be on answer space of subarray sum
-        // Smallest possible subarray sum will be equal to the largest element in the array (when k = n)
-        // Largest possible subarray sum will be equal to the sum of all elements in the array (when k=1)
+        // Idea: answer space is sum of subarrays and we want to minimize that sum
+        // For a given largest sum, see if we can make k subarrays
+        // If we can make less that equal to k subarrays, then answer is possible (since it can be split into k)
+        // If we make more than k subarrays then we need to increase largest sum allowed
 
-        // So, for a given 'maxSumAllowed' find out if k partitions can be made or not
-
-        // If less than k partitions can be made, then k partitions can definitely be made, so answer is possible but
-        // decrease maxSumAllowed in the hopes of finding a smaller answer
-
-        // If more than k partitions can be made, that means the maxSumAllowed is too small and hence should be increased
-
-        long long low = maxElement(nums);
-        long long high = totalSum(nums);
-        long long ans = high;
+        int n = nums.size();
+        int maxSum = 0;
+        
+        int low = maxElement(nums);
+        int high = sumElements(nums);
+        int ans = high;
 
         while (low <= high) {
-            long long mid = low + (high-low)/2;
-            if (canSplitIntoKSubarrays(nums, k, mid)) {
+            int mid = low + (high-low)/2;
+
+            if (canParitionIntoKSubarraysWithGivenMaxSum(nums, k, mid)) {
                 ans = mid;
-                high = mid-1;
-            } else {
-                low = mid+1;
+                high = mid - 1;
+            }
+
+            else {
+                low = mid + 1;
             }
         }
         return ans;
     }
 
-    bool canSplitIntoKSubarrays(vector<int>& nums, int k, long long maxSumAllowed) {
-        // Greedily start the first subarray from 0th element
-        int noOfSubarrays = 1;
-        long long sum  = 0;
-        int i=0;
-        while (i<nums.size()) {
+    bool canParitionIntoKSubarraysWithGivenMaxSum(vector<int>& nums, int k, int maxSumAllowed) {
+        int noOfParitions = 1;
+        int sum = 0;
+
+        int i = 0;
+        while (i < nums.size()) {
             sum += nums[i];
             if (sum <= maxSumAllowed) {
                 i++;
             } else {
-                noOfSubarrays++;
-                if (noOfSubarrays > k) {
+                noOfParitions++;
+                if (noOfParitions > k) {
                     return false;
                 }
                 sum = nums[i];
                 i++;
             }
         }
-        return true;
-    }
 
-    long long maxElement(vector<int>& nums) {
-        int maxElem = INT_MIN;
-        for (int i: nums) {
-            maxElem = max(maxElem, i);
+        if (noOfParitions <= k) {
+            return true;
         }
-        return (long long) maxElem;
+        return false;
     }
 
-    long long totalSum(vector<int>& nums) {
-        long long sum = 0;
-        for (int i: nums) {
+    int maxElement(vector<int>& nums) {
+        int maxElem = nums[0];
+        for (int i:nums) {
+            maxElem = max(i, maxElem);
+        }
+        return maxElem;
+    }
+
+    int sumElements(vector<int>& nums) {
+        int sum = 0;
+        for (int i:nums) {
             sum += i;
         }
         return sum;
     }
-};
+}; 
