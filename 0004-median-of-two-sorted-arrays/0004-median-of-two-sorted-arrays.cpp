51613a14266a79245e87da76047abbe9c96a47d6
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-26 16:57:55 -0600

    Time: 0 ms (100%), Space: 95.1 MB (61.63%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Idea: BS over how many elements we should take from the first array inorder to form a globally sorted
-        // combination.
-
         int m = nums1.size();
         int n = nums2.size();
-        int total = m+n;
 
-        if (m>n) return findMedianSortedArrays(nums2, nums1);
+        int total = m+n;
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
 
         int low = -1;
         int high = m-1;
 
         while (low <= high) {
-            int idxOfLastElementFromNums1 = low + (high-low)/2;
-            int idxOfLastElementFromNums2 = ((total+1)/2) - (idxOfLastElementFromNums1 + 2);
+            int cut1 = low + (high-low)/2; // last index in left half from nums1
+            int cut2 = (total+1)/2 - (cut1 + 1) - 1; // last index in left half from nums2
 
-            int l1 = (idxOfLastElementFromNums1 == -1) ? INT_MIN : nums1[idxOfLastElementFromNums1];
-            int l2 = (idxOfLastElementFromNums2 == -1) ? INT_MIN : nums2[idxOfLastElementFromNums2];
-            int r1 = (idxOfLastElementFromNums1 == m-1) ? INT_MAX : nums1[idxOfLastElementFromNums1+1];
-            int r2 = (idxOfLastElementFromNums2 == n-1) ? INT_MAX : nums2[idxOfLastElementFromNums2+1];
+            int l1 = (cut1 == -1) ? INT_MIN : nums1[cut1];
+            int l2 = (cut2 == -1)  ? INT_MIN : nums2[cut2];
+            int r1 = (cut1 == m-1) ? INT_MAX: nums1[cut1+1];
+            int r2 = (cut2 == n-1) ? INT_MAX: nums2[cut2+1];
 
             if (l1 <= r2 && l2 <= r1) {
                 if (total%2 == 0) {
-                    return (double)(max(l1,l2) + min(r1,r2))/2.0;
+                    return (double) (max(l1, l2) + min(r1, r2))/2;
                 } else {
-                    return max(l1,l2);
+                    return max(l1, l2);
                 }
-            } else if (l1 > r2) {
-                high = idxOfLastElementFromNums1-1;
-            } else {
-                low = idxOfLastElementFromNums1+1;
             }
+
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            }
+
+            else if (l2 > r1) {
+                low = cut1 + 1;
+            }
+
         }
-        return 0.0;
+        return -1;
     }
 };
