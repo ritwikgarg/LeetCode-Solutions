d9f6384ebcd17553d5b92b3fedc4f4ea3e5295ef
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-01-28 16:15:09 -0600

    Time: 227 ms (65.65%), Space: 10.7 MB (61.6%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,12 +3,11 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int rows = board.size();
         int cols = board[0].size();
-
-        if (word.length() > rows*cols) return false;
+        vector<vector<bool>> visited (rows, vector<bool> (cols, false));
 
         for (int i=0; i<rows; i++) {
             for (int j=0; j<cols; j++) {
-                if(exploreBoard(board, word, i, j, 0, i, j)) {
+                if (dfs(board, word, i, j, 0, visited, rows, cols)) {
                     return true;
                 }
             }
@@ -16,44 +15,23 @@ public:
         return false;
     }
 
-    bool exploreBoard(vector<vector<char>>& board, string& word, int currRow, int currCol, int currIndex, int prevRow, int prevCol) {
+    bool dfs(vector<vector<char>>& board, string& word, int currRow, int currCol, int currIndex, vector<vector<bool>>& visited, int rows, int cols) {
         if (currIndex == word.size()) {
             return true;
-        }
-        if (board[currRow][currCol] == word[currIndex]) {
-            if ((currRow == board.size() - 1 && currCol == board[0].size()-1 && currIndex == word.size()-1) || (currRow == 0 && currCol == 0 && currIndex == word.size()-1)) {
-                return true;
-            }
-            if (currRow - 1 >= 0) {
-                if (!(currRow-1 == prevRow && currCol == prevCol)) {
-                    if (exploreBoard(board, word, currRow-1, currCol, currIndex+1, currRow, currCol)) {
-                        return true;
-                    } // Search top
-                }
-            }
-            if (currRow + 1 < board.size()) {
-                if (!(currRow+1 == prevRow && currCol == prevCol)) {
-                    if (exploreBoard(board, word, currRow+1, currCol, currIndex+1, currRow, currCol)) {
-                        return true;
-                    } // Search bottom
-                }
-            }
-            if (currCol -1 >= 0) {
-                if (!(currRow == prevRow && currCol-1 == prevCol)) {
-                    if (exploreBoard(board, word, currRow, currCol-1, currIndex+1, currRow, currCol)) {
-                        return true;
-                    } // Search left
-                }
-            }
-            if (currCol + 1 < board[0].size()) {
-                if (!(currRow == prevRow && currCol+1 == prevCol)) {
-                    if (exploreBoard(board, word, currRow, currCol+1, currIndex+1, currRow, currCol)) {
-                        return true;
-                    } // Search right
-                }
-            }
+        } 
+
+        if (currRow < 0 || currRow >= rows || currCol < 0 || currCol >= cols || board[currRow][currCol] != word[currIndex] ||visited[currRow][currCol] == true) {
+            return false;
         }
 
-        return false;
+        visited[currRow][currCol] = true;
+        bool res = dfs(board, word, currRow+1, currCol, currIndex+1, visited, rows, cols ) || 
+        dfs(board, word, currRow-1, currCol, currIndex+1, visited, rows, cols ) ||
+        dfs(board, word, currRow, currCol+1, currIndex+1, visited, rows, cols ) ||
+        dfs(board, word, currRow, currCol-1, currIndex+1, visited, rows, cols );
+
+        visited[currRow][currCol] = false;
+
+        return res;
     }
 };
