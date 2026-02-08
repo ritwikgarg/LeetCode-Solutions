ed205614e227265af5168299b0f630f496adee71
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-02-08 13:31:02 -0600

    Time: 17 ms (80.3%), Space: 139.1 MB (66.93%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // Idea: maintain a doubly-ended queue, wich acts as a monotonic stack (largest element at bottom, smallest
-        // element at top). Whenever we exceed window size, pop from the bottom. Always push from the top, as long
-        // as element being pushed is smaller than element currently at top.
-
-        deque<int> dqIdx;
+        deque<int> dq;
         int n = nums.size();
         vector<int> ans;
 
         for (int i=0; i<n; i++) {
-            while (!dqIdx.empty() && nums[dqIdx.back()] < nums[i]) {
-                dqIdx.pop_back();
-            }
-            if (!dqIdx.empty() && dqIdx.front() < i-k+1) {
-                dqIdx.pop_front();
-            }
+
+            if (!dq.empty() && i - dq.front() >= k) {
+                dq.pop_front();
+            } 
+
+            while(!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            } 
+
+            dq.push_back(i);
+
             if (i >= k-1) {
-                int largestElementInWindowIdx = (dqIdx.empty()) ? i : dqIdx.front();
-                ans.push_back(nums[largestElementInWindowIdx]);
+                ans.push_back(nums[dq.front()]);
             }
-            dqIdx.push_back(i);
         }
+
         return ans;
     }
 };
