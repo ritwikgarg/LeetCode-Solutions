b1c7572e0b1ed88468639857071eefe6af1720a0
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-03-13 20:23:51 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -3,9 +3,11 @@ public:
     string getPermutation(int n, int k) {
         vector<char> digitsAvailable;
         string ans = "";
-        for (int i=1; i<=n; i++) {
-            digitsAvailable.push_back('0'+i);
+
+        for (int i = 1; i <= n; i++) {
+            digitsAvailable.push_back('0' + i);
         }
+
         buildAns(ans, n, k, digitsAvailable);
         return ans;
     }
@@ -15,23 +17,24 @@ public:
             ans += digitsAvailable[0];
             return;
         }
-        int blockSize = factorial(n-1);
 
-        int digitExcludedIndex = ((k-1)/blockSize);
+        int blockSize = factorial(n - 1);
+        int index = (k - 1) / blockSize;
 
-        int digitExcluded = digitsAvailable[digitExcludedIndex];
-        digitsAvailable.erase(digitsAvailable.begin() + digitExcludedIndex);
+        char digit = digitsAvailable[index];
+        digitsAvailable.erase(digitsAvailable.begin() + index);
 
-        ans += digitExcluded;
-        k = (k-1) % blockSize + 1;
+        ans += digit;
 
-        buildAns(ans, n-1, k, digitsAvailable);
+        k = (k - 1) % blockSize + 1;
+
+        buildAns(ans, n - 1, k, digitsAvailable);
     }
 
     int factorial(int n) {
         int prod = 1;
-        for (int i=n; i>=1; i--) {
-            prod*=i;
+        for (int i = n; i >= 1; i--) {
+            prod *= i;
         }
         return prod;
     }
