14fc043793467e984ffceb6524f94448150b723f
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-03-13 21:44:32 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,34 +3,18 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
 
-        // Answer must lie within 1 and n+1
-
-        // Pass 1: normalize all elements to n+1 if they are <= 0 and > n
         for (int i=0; i<n; i++) {
-            if (nums[i] <= 0 || nums[i] > n) {
-                nums[i] = n+1;
+            while ((nums[i] > 0 && nums[i] <= n) && (nums[i] - 1 != i)) {
+                int index = nums[i] - 1;
+                swap(nums[i], nums[index]);
             }
         }
 
-        // Pass 2: mark corresponding index as negative to show pressence
-        for (int i=0; i<n; i++) {
-            int num = abs(nums[i]);
-            if (num > 0 && num <= n) {
-                int index = num-1;
-                if (nums[index] < 0) continue; 
-                nums[index] = -nums[index];
-            }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
         }
 
-        // Pass 3: go through array to find first non-negative index
-        int ans = n+1;
-        for (int i=0; i<n; i++) {
-            if (nums[i] > 0) {
-                ans = i+1;
-                break;
-            }
-        }
-
-        return ans;
+        return n+1;
     }
 };
