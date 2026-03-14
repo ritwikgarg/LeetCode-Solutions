a33062456eda72c57ae4677f229e1a3fe7d49ea2
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-03-13 19:06:02 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if (numRows == 1) {
-            return s;
-        }
-        int n = s.size();
-        vector<string> matrix(numRows);
-
-        int row = 0;
-        int direction=1;
-
-        for (int i=0; i<n; i++) {
-            matrix[row] += s[i];
-
-            if (row == numRows-1) {
-                direction = -1;
-            } 
-            if (row == 0) {
-                direction = 1;
-            }
-
-            row += direction;
-            
-        }
-
-        string ans = "";
+        vector<string> ans(numRows);
+        dfs(s, 0, 0, 1, numRows, ans);
+        
+        string answer = "";
         for (int i=0; i<numRows; i++) {
-            ans+=matrix[i];
+            answer += ans[i];
         }
 
-        return ans;
+        return answer;
+    }
+
+    void dfs(string& s, int index, int currRow, int direction, int numRows, vector<string>& ans) {
+        if (index == s.size()) {
+            return;
+        }
+
+        ans[currRow] += s[index];
+
+        if (currRow == numRows-1) {
+            direction = -1;
+        }
+        if (currRow == 0) {
+            direction = 1;
+        }
+
+        currRow += direction;
+
+        dfs(s, index+1, currRow, direction, numRows, ans);
     }
 };
