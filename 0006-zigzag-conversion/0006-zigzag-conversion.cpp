4465fa578929b71bc9aa82dbb352c0c76cd89621
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-03-13 19:09:57 -0500

    Time: 0 ms (100%), Space: 13.9 MB (69.05%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,33 +1,36 @@
-class Solution {
-public:
-    string convert(string s, int numRows) {
-        vector<string> ans(numRows);
-        dfs(s, 0, 0, 1, numRows, ans);
-        
-        string answer = "";
-        for (int i=0; i<numRows; i++) {
-            answer += ans[i];
+    class Solution {
+    public:
+        string convert(string s, int numRows) {
+            if (numRows == 1) {
+                return s;
+            }
+            vector<string> ans(numRows);
+            dfs(s, 0, 0, 1, numRows, ans);
+            
+            string answer = "";
+            for (int i=0; i<numRows; i++) {
+                answer += ans[i];
+            }
+
+            return answer;
         }
 
-        return answer;
-    }
+        void dfs(string& s, int index, int currRow, int direction, int numRows, vector<string>& ans) {
+            if (index == s.size()) {
+                return;
+            }
 
-    void dfs(string& s, int index, int currRow, int direction, int numRows, vector<string>& ans) {
-        if (index == s.size()) {
-            return;
+            ans[currRow] += s[index];
+
+            if (currRow == numRows-1) {
+                direction = -1;
+            }
+            if (currRow == 0) {
+                direction = 1;
+            }
+
+            currRow += direction;
+
+            dfs(s, index+1, currRow, direction, numRows, ans);
         }
-
-        ans[currRow] += s[index];
-
-        if (currRow == numRows-1) {
-            direction = -1;
-        }
-        if (currRow == 0) {
-            direction = 1;
-        }
-
-        currRow += direction;
-
-        dfs(s, index+1, currRow, direction, numRows, ans);
-    }
-};
+    };
