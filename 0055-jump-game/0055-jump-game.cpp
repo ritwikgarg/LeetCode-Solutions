e54d54e2381c60a0fc1ce82483861e65a9a5254d
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-03-16 14:49:55 -0500

    Time: 4 ms (21.83%), Space: 52.3 MB (56.56%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxIndex = 0;
+
+        for (int i=0; i<n; i++) {
+            if (i <= maxIndex) {
+                maxIndex = max(maxIndex, i + nums[i]);
+                if (maxIndex >= n-1) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
