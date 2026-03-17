3976f90d9789869dee33901524483083caabfd7e
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-03-17 18:28:46 -0500

    Time: 0 ms (100%), Space: 8.6 MB (53.89%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int reverse(int x) {
-        int reverseNo = 0;
-        bool isNegative = (x < 0) ? true : false;
-        
-        if (x < -INT_MAX) return 0;
-
-        int temp = abs(x);
-        while (temp!=0) {
-            int digit = temp%10;
-            temp = temp/10;
-            if (reverseNo > (INT_MAX - digit)/10) {
-                return 0;
+       int ans = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            // Check for overflow/underflow before updating ans
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && digit > INT_MAX % 10)) {
+                return 0; // Return 0 if reversing x would cause overflow
             }
-            reverseNo = reverseNo * 10 + digit;
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && digit < INT_MIN % 10)) {
+                return 0; // Return 0 if reversing x would cause underflow
+            }
+            ans = ans * 10 + digit;
+            x = x / 10;
         }
-        return (isNegative) ? -reverseNo : reverseNo;
+        return ans;
     }
 };
