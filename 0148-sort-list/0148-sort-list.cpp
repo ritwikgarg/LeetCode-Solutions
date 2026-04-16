3a9aa3a46789b93219012b0675851324074726c9
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-04-16 18:38:23 -0500

    Time: 12 ms (72.61%), Space: 57.1 MB (82.07%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+        
+        ListNode* rightHalf = splitHalf(head);
+        head = sortList(head);
+        rightHalf = sortList(rightHalf);
+
+        return merge(head, rightHalf);
+    }
+
+    ListNode* merge(ListNode* leftHalf, ListNode* rightHalf) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (leftHalf && rightHalf) {
+            if (leftHalf->val < rightHalf->val) {
+                tail->next = leftHalf;
+                leftHalf = leftHalf->next;
+            } else {
+                tail->next = rightHalf;
+                rightHalf = rightHalf->next;
+            }
+            tail = tail->next;
+        }
+
+        // Drain out remaining nodes
+        while(leftHalf) {
+            tail->next = leftHalf;
+            leftHalf = leftHalf->next;
+            tail = tail->next;
+        }
+        
+        while(rightHalf) {
+            tail->next = rightHalf;
+            rightHalf = rightHalf->next;
+            tail = tail->next;
+        }
+
+        return dummy.next;
+    }
+
+    ListNode* splitHalf(ListNode* head) {
+        if (!head || !head->next) {
+            return nullptr;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
+        return mid;
+    }
+};
