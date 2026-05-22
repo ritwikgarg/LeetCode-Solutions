819b5dce238e1c80b1e96f312ab3437a388d57e1
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-22 03:33:23 -0500

    Time: 0 ms (100%), Space: 17 MB (89.79%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,29 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if (!root) return ans;
+        levelorder(root, 0, ans);
+        return ans;
+    }
 
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-            vector<int> level;
-
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
-                level.push_back(node->val);
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-
-            ans.push_back(level);
+    void levelorder(TreeNode* node, int level, vector<vector<int>>& ans) {
+        if (node == nullptr) {
+            return;
         }
 
-        return ans;
+        // Check if curr node is the first node we have seen for this level
+        if(level == ans.size()) {
+            ans.push_back({});
+        }
+        ans[level].push_back(node->val);
+
+        levelorder(node->left, level+1, ans);
+        levelorder(node->right, level+1, ans);
     }
 };
