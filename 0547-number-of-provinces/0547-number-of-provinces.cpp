a834a9d9d572bfa9a9e23ea44d230bc697351a9a
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-24 03:29:30 -0500

    Time: 0 ms (100%), Space: 19.5 MB (61.32%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -2,53 +2,29 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        if (n==0) return 0;
+        vector<int> visited(n, 0);
 
         int count = 0;
-        vector<int> visited(n,0);
 
-        while(true) {
-            int currNode = findNextUnvisited(visited);
-            count++;
-            DFS(currNode, isConnected, visited);
-            if (allNodesVisited(visited)) return count;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i, isConnected, visited);
+            }
         }
+
         return count;
     }
 
-    void DFS(int node, vector<vector<int>>& adjMatrix, vector<int>& visited) {
-        visited[node] = 1; // Mark curr node as visited
-        vector<int> neighbors = findNeighbors(node, adjMatrix);
+    void DFS(int node, vector<vector<int>>& isConnected, vector<int>& visited) {
+        visited[node] = 1;
 
-        for (int it: neighbors) {
-            if (!visited[it]) {
-                DFS(it, adjMatrix, visited);
+        int n = isConnected.size();
+
+        for (int neighbor = 0; neighbor < n; neighbor++) {
+            if (isConnected[node][neighbor] == 1 && !visited[neighbor]) {
+                DFS(neighbor, isConnected, visited);
             }
         }
-    }
-
-    bool allNodesVisited(vector<int>& visited) {
-        for (int it: visited) {
-            if (it == 0) return false;
-        }
-        return true;
-    }
-
-    int findNextUnvisited(const vector<int>& visited) {
-        for (int i = 0; i < visited.size(); i++) {
-            if (visited[i] == 0) return i;
-        }
-        return -1;
-    }
-
-    vector<int> findNeighbors(int node, vector<vector<int>>& adjMatrix) {
-        vector<int> neighbors;
-        int n = adjMatrix.size();
-
-        for (int i=0; i<n; i++) {
-            if (adjMatrix[node][i] == 1) neighbors.push_back(i);
-        }
-
-        return neighbors;
     }
 };
