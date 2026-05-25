15f216e2f05eb882da974faabbe6c9f8f56f69cc
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-25 17:06:46 -0500

    Time: 0 ms (100%), Space: 8.5 MB (33.11%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        // Using memoization
-        // dp[i] = number of ways to reach step 0 starting from step i
+        // Using memoization -> top-down recursion + caching
+        // So we want to start from dfs(n) and go down to base case
         vector<int> dp(n+1, -1);
-        dp[0] = 1; // one way to be at 0: do nothing
-        climb(n, dp);
-        return dp[n];
+        return dfs(n, dp);
     }
 
-    void climb(int n, vector<int>& dp) {
-        if (n == 0) {
-            dp[n] = 0;
-            return;
-        }
-        if (n == 1) {
-            dp[n] = 1;
-            return;
-        }
+    int dfs(int n, vector<int>& dp) {
+        if (n == 0 || n == 1) return 1;
 
-        if (dp[n-1] == -1) climb(n-1, dp);
-        if (dp[n-2] == -1) climb(n-2, dp);
+        if (dp[n] != -1) return dp[n];
 
-        dp[n] = dp[n-1] + dp[n-2];
+        dp[n] = dfs(n-1, dp) + dfs(n-2, dp);
+
+        return dp[n];
     }
 };
