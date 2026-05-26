3c4a6fec90a031199486b6b1583135ca46eb27ac
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-26 18:29:03 -0500

    Time: 303 ms (27.07%), Space: 69.9 MB (5.01%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,20 +2,28 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        return findWays(0, n, nums, 0, target);
+        // Using memoization
+        map<pair<int, int>, int> dp; // stores (index, val) , no. of ways
+        return findWays(0, n, nums, 0, target, dp);
     }
 
-    int findWays(int i, int n, vector<int>& nums, int currVal, int target) {
+    int findWays(int i, int n, vector<int>& nums, int currVal, int target, map<pair<int, int>, int>& dp) {
+        
         if (i == n) {
-            if (currVal == target) return 1;
+            if (currVal == target) {
+                dp[{i, currVal}] = 1;
+                return 1;
+            }
+            dp[{i, currVal}] = 0;
             return 0;
         }
-        
-        int number = nums[i];
 
-        int positive = findWays(i+1, n, nums, currVal+number, target);
-        int negative = findWays(i+1, n, nums, currVal-number, target);
+        if (dp.find({i, currVal}) != dp.end()) return dp[{i, currVal}];
 
-        return positive+negative;
+        int positive = findWays(i+1, n, nums, currVal + nums[i], target, dp);
+        int negative = findWays(i+1, n, nums, currVal - nums[i], target, dp);
+
+        dp[{i, currVal}] = positive + negative;
+        return positive + negative;
     }
 };
