4dcc81b35a77fae1b0508a7bb9a89446dcae2cb1
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-26 16:14:06 -0500

    Time: 168 ms (23.07%), Space: 17.4 MB (57.9%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -3,37 +3,30 @@ public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
 
-        int totalSum = 0;
+        int totSum = 0;
+        for (int i=0; i<n; i++) totSum += nums[i];
+
+        if (totSum % 2 != 0) return false;
+        int target = totSum/2;
+
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, false));
+
         for (int i=0; i<n; i++) {
-            totalSum += nums[i];
+            dp[i][0] = true; // by not picking the element at index i we can always find target = 0
+
+            if (nums[i] <= target) dp[i][nums[i]] = true; // by picking the element at index i we can always find target = nums[i]
         }
 
-        if (totalSum%2 != 0) return false;
-        int target = totalSum/2;
+        for (int i=1; i<n; i++) {
+            for (int sum=1; sum<=target; sum++) {
+                bool dontTakeAtI = dp[i-1][sum];
+                bool takeAtI = false;
+                if (sum - nums[i] >= 0)  takeAtI = dp[i-1][sum - nums[i]];
 
-        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+                dp[i][sum] = takeAtI | dontTakeAtI;
+            }
+        }
 
-        return partition(n-1, 0, nums, target, dp);
-    }
-
-    bool partition(int i, int currSum, vector<int>& nums, int target, vector<vector<int>>& dp) {
-        if (currSum == target) return true;
-        if (i < 0 || currSum > target) return false;
-
-        if (dp[i][currSum] != -1) return dp[i][currSum];
-
-        // Take i
-        bool takeI = partition(i-1, currSum+nums[i], nums, target, dp);
-
-        // Dont take i
-        bool dontTakeI = partition(i-1, currSum, nums, target, dp);
-
-        if (takeI || dontTakeI) {
-            dp[i][currSum] = true;
-            return dp[i][currSum];
-        } 
-
-        dp[i][currSum] = false;
-        return dp[i][currSum];
+        return dp[n-1][target];
     }
 };
