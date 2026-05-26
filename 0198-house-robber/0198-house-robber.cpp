e1b72ce79a0421b0155372ee73092d1507f17518
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-25 20:46:34 -0500

    Time: 0 ms (100%), Space: 10.8 MB (20.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        // Using memoization
+        // Using tabulation
         int n = nums.size();
+        if (n==0) return 0;
+        if (n==1) return nums[0];
+        
         vector<int> dp(n, INT_MIN);
-        return robHouse(n-1, nums, dp);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+
+        for (int i=2; i<n; i++) {
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        }
+
+        return dp[n-1];
     }
 
-    int robHouse(int n, vector<int>& nums, vector<int>& dp) {
-        if (n == 0) return nums[0];
-        if (n == 1) return max(nums[0], nums[1]);
-
-        if (dp[n] != INT_MIN) return dp[n];
-
-        int robCurrent = nums[n] + robHouse(n-2, nums, dp);
-        int skipCurrent = robHouse(n-1, nums, dp);
-
-        dp[n] = max(skipCurrent, robCurrent);
-        return dp[n];
-    }
 
 };
