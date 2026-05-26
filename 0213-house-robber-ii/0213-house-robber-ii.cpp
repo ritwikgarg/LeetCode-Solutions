8237aabb5d47de6f7fbfa3bffef081d14283a6f5
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-25 21:28:41 -0500

    Time: 0 ms (100%), Space: 11.1 MB (7.79%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        vector<int> temp1;
+        vector<int> temp2;
+
+        for (int i=0; i<n; i++) {
+            if (i!=n-1) temp1.push_back(nums[i]);
+            if (i!=0) temp2.push_back(nums[i]);
+        }
+
+
+        vector<int> dp1(n-1, INT_MIN);
+        vector<int> dp2(n-1, INT_MIN);
+        int skippingLastHouse = robHouse(n-2, temp1, dp1);
+        int skippingFirstHouse = robHouse(n-2, temp2, dp2);
+
+        return max(skippingFirstHouse, skippingLastHouse);
+    }
+
+    int robHouse(int i, vector<int>& nums, vector<int>& dp) {
+        if (i==0) return nums[0];
+        if (i==1) return max(nums[0], nums[1]);
+
+        if (dp[i] != INT_MIN) return dp[i];
+
+        int skipCurr = robHouse(i-1, nums, dp);
+        int robCurr = nums[i] + robHouse(i-2, nums, dp);
+
+        dp[i] = max(skipCurr, robCurr);
+        return dp[i];
+    }
+};
