3be6b9c2cfd3afeb41fc31e148257a8e9029e6ac
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-25 21:34:25 -0500

    Time: 0 ms (100%), Space: 10.9 MB (36.19%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -27,14 +27,16 @@ public:
         if (i==0) return nums[0];
         if (i==1) return max(nums[0], nums[1]);
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
 
         int n=nums.size();
         for (int i=2; i<n; i++) {
-            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+            int curr = max(prev1, nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n-1];
+        return prev1;
     }
 };
