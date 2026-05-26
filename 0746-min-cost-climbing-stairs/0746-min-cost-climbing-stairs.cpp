eeeade7069ed1ebdf6aabce462fa5c24041261d6
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-25 19:13:06 -0500

    Time: 0 ms (100%), Space: 17.8 MB (27.68%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -4,13 +4,15 @@ public:
         int n = cost.size();
         if (n==0 || n==1) return 0;
         vector<int> dp(n+1, INT_MAX);
-        dp[0] = 0;
-        dp[1] = 0;
+        int prev2 = 0;
+        int prev1 = 0;
 
         for (int i=2; i<=n; i++) {
-            dp[i] = min((dp[i-1] + cost[i-1]), (dp[i-2] + cost[i-2]));
+            int curr = min((prev1 + cost[i-1]), (prev2 + cost[i-2]));
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
     }
 };
