e1b47c71bf4dfb96c62516be4dc3af88a2116d9a
Ritwik Garg <81905343+ritwikgarg@users.noreply.github.com>
2026-05-27 22:06:54 -0500

    Time: 339 ms (7.49%), Space: 21.9 MB (14.63%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+
+        while (i < intervals.size()) {
+            // end of an interval is smaller than start of new interval
+            if (intervals[i][1] < newInterval[0]) {
+                i++;
+            }
+
+            // start of an interval is larger than end of new interval
+            else if (intervals[i][0] > newInterval[1]) {
+                intervals.insert(intervals.begin() + i, newInterval);
+                return intervals;
+            }
+
+            // merge the intervals and erase the existing interval
+            else {
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+
+                intervals.erase(intervals.begin() + i);
+            }
+        }
+
+        intervals.push_back(newInterval);
+        return intervals;
+    }
+};
